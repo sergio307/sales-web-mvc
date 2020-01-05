fda2ee0d10fe13e8856753834bc05b1ae5fc5712
sergio307 <sergio.henrique04@gmail.com>
2020-01-05 11:53:55 -0300

    MySQL adaptation and first migration

--- SalesWebMvc/Controllers/DepartamentsController.cs
+++ SalesWebMvc/Controllers/DepartamentsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Controllers
